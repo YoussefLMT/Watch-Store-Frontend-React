dbd29a8eec370f2f9d8887a1b0d206817c35f928
YoussefLMT <mcyoussef324@gmail.com>
2022-11-21 19:39:48 +0100

    add clicked state

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from '../logo.svg'
+import './navbar.css'
 
 function Navbar() {
+
+  const [clicked, setCicked] = useState(false)
+
   return (
     <>
       <nav>
