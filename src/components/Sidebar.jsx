eebe564839cae8785e69f4cfef58ef1f465b2f67
YoussefLMT <mcyoussef324@gmail.com>
2022-11-23 18:28:21 +0100

    add clicked state

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './sidebar.css'
 
 function Sidebar() {
+
+    const[clicked, setClicked] = useState(false)
+
+   
     
     return (
-        <div className="sidebar" style={{ width:' 20%'}}>
+        <div className="sidebar" style={{ width: '20%'}}>
             Sidebar 
             <br />
 
-            <button>toggle</button>
+            <button onClick={toggle}>toggle</button>
         </div>
     )
 }
