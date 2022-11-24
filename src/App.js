57e9c6db105ace6d37835e6afffdb7a3d33db327
YoussefLMT <mcyoussef324@gmail.com>
2022-11-24 18:08:33 +0100

    modification

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/navbar/Navbar";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Dashboard />
     </div>
   );
 }
