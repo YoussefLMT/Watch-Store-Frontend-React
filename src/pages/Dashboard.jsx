b9c75b8c1805675f8eb6458a74c9b52d24b39e5c
YoussefLMT <mcyoussef324@gmail.com>
2022-11-24 18:19:57 +0100

    modifications

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Sidebar from '../components/Sidebar'
 import './dashboard.css'
 
 function Dashboard() {
-
-    const [clicked, setCicked] = useState(false)
-
     return (
         <div className='p'>
             <Sidebar />
             <main>
                 Dashboard
-
-                <div className='mobile' onClick={() => setCicked(clicked => !clicked)}>
-                    <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                </div>
             </main>
         </div>
     )
