b0e9cf27c1f2d4e7db26a1706cdffb08d37f7b79
YoussefLMT <mcyoussef324@gmail.com>
2022-11-28 18:25:45 +0100

    some additions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,25 @@ import Navbar from "../components/navbar/Navbar"
 import './home.css'
 
 function Home() {
-  return (
-    <>
-      <Navbar />
-      <div className="home">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-4">
-                    <h2>Choose your watch now!</h2>
+    return (
+        <>
+            <Navbar />
+            <div className="home">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4 content">
+                            <h2>Choose your watch now!</h2>
+                            <p className="mt-3">Lorem ipsum dolor sit amet consectetur adipisicing
+                                elit. Delectus assumenda temporibus dolor impedit autem esse in culpa
+                                nulla quos magnam veniam reprehenderit enim, perferendis doloremque,
+                                maiores ipsa laudantium iure
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-      </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Home
