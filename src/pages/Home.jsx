e354fa569f59014e229fa5a969851a5e8ccf5c9b
YoussefLMT <mcyoussef324@gmail.com>
2022-11-28 18:15:33 +0100

    add some style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import Navbar from "../components/navbar/Navbar";
+import Navbar from "../components/navbar/Navbar"
+import './home.css'
 
 function Home() {
   return (
     <>
       <Navbar />
+      <div className="home">
+
+      </div>
     </>
   )
 }
