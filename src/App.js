de40b88c48dc94da2c315e81d9a17a186961aeef
YoussefLMT <mcyoussef324@gmail.com>
2022-11-28 18:12:49 +0100

    add navbar to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Navbar from "./components/navbar/Navbar";
-import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Home />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from "../components/navbar/Navbar";
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
