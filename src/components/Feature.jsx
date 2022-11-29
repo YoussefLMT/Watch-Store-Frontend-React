32864daefdd83575f2bc5fb134e725966a209078
YoussefLMT <mcyoussef324@gmail.com>
2022-11-29 19:49:27 +0100

    add content

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function Feature() {
-  return (
-    <div>Feature</div>
-  )
+function Feature(props) {
+    return (
+        <div class="box">
+            <div class="imgbox">
+                <img src={props.image} />
+            </div>
+            <div class="text">
+                <h3>{ props.name }</h3>
+                <p>{ props.description }</p>
+            </div>
+        </div>
+    )
 }
 
 export default Feature
