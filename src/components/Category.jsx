e7aab98bed2897250fcb146f3cc552806f9b53ff
YoussefLMT <mcyoussef324@gmail.com>
2022-11-29 20:25:38 +0100

    add content

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-function Category() {
-  return (
-    <div>Category</div>
-  )
+function Category(props) {
+    return (
+        <div class="box">
+            <div class="imgbox">
+                <img src={props.image} />
+            </div>
+            <div class="text">
+                <h3>{props.name}</h3>
+            </div>
+        </div>
+    )
 }
 
 export default Category
