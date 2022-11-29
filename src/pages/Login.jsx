be53306ab3b663deae5c6400735a958ba15a9820
YoussefLMT <mcyoussef324@gmail.com>
2022-11-29 21:50:12 +0100

    add login content

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <>
+            <Navbar />
+            <div class="auth-content">
+                <form>
+                    <h2 class="form-title">Register</h2>
+                    <div>
+                        <label>Email</label>
+                        <input type="email" class="text-input" />
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input type="password" class="text-input" />
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-big">Register</button>
+                    </div>
+                    <p>Or <Link to="/login">Login</Link>
+                    </p>
+                </form>
+            </div>
+        </>
+    )
 }
 
 export default Login
