ec9806795f5c34470cb68a0aa385f9066c308761
YoussefLMT <mcyoussef324@gmail.com>
2022-11-30 19:16:18 +0100

    add register function

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import './register.css'
+import axiosInstance from '../axios'
 
 function Register() {
 
@@ -17,26 +18,44 @@ function Register() {
         setForm({ ...form, [e.target.name]: e.target.value });
     }
 
+    const register = async (e) => {
+        e.preventDefault();
+
+        const data = {
+            name: form.name,
+            email: form.email,
+            password: form.password
+        }
+
+        const response = await axiosInstance.post('/register', data)
+
+        if (response.data.status === 200) {
+            console.log(response.data.message)
+        } else {
+            setForm({ ...form, error_list: response.data.validation_err });
+        }
+    }
+
     return (
         <>
             <Navbar />
             <div class="auth-content">
-                <form>
-                    <h2 class="form-title">Register</h2>
+                <form onSubmit={register}>
+                    <h2 className="form-title">Register</h2>
                     <div>
                         <label>Name</label>
-                        <input type="text" value={form.name} onChange={handleChange} class="text-input" />
+                        <input type="text" name="name" value={form.name} onChange={handleChange} className="text-input" />
                     </div>
                     <div>
                         <label>Email</label>
-                        <input type="email" value={form.email} onChange={handleChange} class="text-input" />
+                        <input type="email" name="email" value={form.email} onChange={handleChange} className="text-input" />
                     </div>
                     <div>
                         <label>Password</label>
-                        <input type="password" value={form.password} onChange={handleChange} class="text-input" />
+                        <input type="password" name="password" value={form.password} onChange={handleChange} className="text-input" />
                     </div>
                     <div>
-                        <button type="button" class="btn btn-big">Register</button>
+                        <button type="submit" className="btn btn-big">Register</button>
                     </div>
                     <p>Or <Link to="/login">Login</Link>
                     </p>
