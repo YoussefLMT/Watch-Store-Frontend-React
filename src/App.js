a8bbdbc92ae3c80ffbfcc7720b4185477ff01537
YoussefLMT <mcyoussef324@gmail.com>
2022-12-02 19:35:16 +0100

    add dashboard route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={localStorage.getItem('token') ? <Navigate to='/' /> : <Login />} />
         <Route path='/register' element={localStorage.getItem('token') ? <Navigate to='/' /> : <Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+
       </Routes>
     </BrowserRouter>
   );
