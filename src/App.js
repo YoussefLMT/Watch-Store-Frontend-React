bf640774161428620070468a57a1f77dfc077e0d
YoussefLMT <mcyoussef324@gmail.com>
2022-12-02 19:29:50 +0100

    prevent accessing login and register pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -8,8 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={localStorage.getItem('token') ? <Navigate to='/' /> : <Login />} />
+        <Route path='/register' element={localStorage.getItem('token') ? <Navigate to='/' /> : <Register />} />
       </Routes>
     </BrowserRouter>
   );
