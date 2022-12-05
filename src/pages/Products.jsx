618425937f7d37123d17f63319c7ecb53b12a4fe
YoussefLMT <mcyoussef324@gmail.com>
2022-12-05 20:00:10 +0100

    add modal

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -18,7 +18,32 @@ function Products() {
                             Add Product
                         </button>
                     </div>
-                </div>    
+                </div>  
+
+
+                {/* Add Car Modal  */}
+                <div className="modal fade" id="addCarModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Add New Car</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    <div className="mb-3">
+                                        <label htmlFor="name" className="form-label">Name</label>
+                                        <input type="text" name='name' className="form-control" id="name" />
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="submit" className="btn btn-primary">Save changes</button>
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
             </main>
         </div>
     )
