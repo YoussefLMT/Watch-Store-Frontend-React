3f4dbfe83c94c1596028d08a075dc957c039fdd3
YoussefLMT <mcyoussef324@gmail.com>
2022-12-05 20:04:10 +0100

    add inputs

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -14,19 +14,19 @@ function Products() {
                         Cars Management
 
                         {/* Button trigger add product modal  */}
-                        <button type="button" className="btn btn-primary btn-add" data-bs-toggle="modal" data-bs-target="#addCarModal">
+                        <button type="button" className="btn btn-primary btn-add" data-bs-toggle="modal" data-bs-target="#addProductModal">
                             Add Product
                         </button>
                     </div>
-                </div>  
+                </div>
 
 
-                {/* Add Car Modal  */}
-                <div className="modal fade" id="addCarModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                {/* Add Product Modal  */}
+                <div className="modal fade" id="addProductModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog">
                         <div className="modal-content">
                             <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">Add New Car</h5>
+                                <h5 className="modal-title" id="exampleModalLabel">Add New Product</h5>
                                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className="modal-body">
@@ -34,6 +34,26 @@ function Products() {
                                     <div className="mb-3">
                                         <label htmlFor="name" className="form-label">Name</label>
                                         <input type="text" name='name' className="form-control" id="name" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="price" className="form-label">Price</label>
+                                        <input type="text" name='price' className="form-control" id="price" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="quantity" className="form-label">Quantity</label>
+                                        <input type="text" name='quantity' className="form-control" id="quantity" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="category" className="form-label">Category</label>
+                                        <input type="text" name='category' className="form-control" id="category" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="description" className="form-label">Description</label>
+                                        <input type="text" name='description' className="form-control" id="description" />
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="image" class="form-label">Image</label>
+                                        <input class="form-control" type="file" id="image" />
                                     </div>
                                     <div className="modal-footer">
                                         <button type="submit" className="btn btn-primary">Save changes</button>
@@ -43,7 +63,7 @@ function Products() {
                             </div>
                         </div>
                     </div>
-                </div>  
+                </div>
             </main>
         </div>
     )
