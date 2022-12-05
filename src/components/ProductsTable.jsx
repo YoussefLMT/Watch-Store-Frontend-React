16e17bd392280ad43ecb4354cce9b93463998cac
YoussefLMT <mcyoussef324@gmail.com>
2022-12-05 21:22:28 +0100

    add table

--- src/components/ProductsTable.jsx
+++ src/components/ProductsTable.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
 function ProductsTable() {
-  return (
-    <div>ProductsTable</div>
-  )
+    return (
+        <>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Quantity</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Image</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                                <tr>
+                                    <th scope="row">66</th>
+                                    <td>tt</td>
+                                    <td>tt</td>
+                                    <td>tt</td>
+                                    <td>tt</td>
+                                    <td>tt</td>
+                                    <td>tt</td>
+                                </tr>
+                         
+                </tbody>
+            </table>
+        </>
+    )
 }
 
 export default ProductsTable
