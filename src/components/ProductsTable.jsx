89f38f5db8e2d7bd039931a12644050e1857be42
YoussefLMT <mcyoussef324@gmail.com>
2022-12-05 21:50:50 +0100

    add update form

--- src/components/ProductsTable.jsx
+++ src/components/ProductsTable.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axiosInstance from '../axios';
 
 function ProductsTable(props) {
@@ -57,10 +57,10 @@ function ProductsTable(props) {
                                     <td>{product.quantity}</td>
                                     <td>{product.category}</td>
                                     <td>{product.description}</td>
-                                    <img src={`http://127.0.0.1:8000/${product.image}`}/>
+                                    <img src={`http://127.0.0.1:8000/${product.image}`} />
                                     <td>
                                         <button type="button" onClick={(e) => deleteProduct(e, product.id)} class="btn btn-danger"><i class="fa-sharp fa-solid fa-trash"></i></button>
-                                        <button type="button" style={{marginLeft: '10px', color: 'white'}} data-bs-toggle="modal" data-bs-target="#updateUserModal" onClick={() => getProduct(product.id)} class="btn btn-warning"><i class="fa-sharp fa-solid fa-pen-to-square"></i></button>
+                                        <button type="button" style={{ marginLeft: '10px', color: 'white' }} data-bs-toggle="modal" data-bs-target="#updateUserModal" onClick={() => getProduct(product.id)} class="btn btn-warning"><i class="fa-sharp fa-solid fa-pen-to-square"></i></button>
                                     </td>
                                 </tr>
                             )
@@ -68,6 +68,46 @@ function ProductsTable(props) {
                     }
                 </tbody>
             </table>
+
+            {/* update product modal */}
+            <div className="modal fade" id="updateProductModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Update Product</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form onSubmit={updateUser}>
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label">Name</label>
+                                    <input type="text" name='name' value={product?.name || ''} onChange={handleChange} className="form-control" id="name" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="price" className="form-label">Price</label>
+                                    <input type="text" name='price' value={product?.price || ''} onChange={handleChange} className="form-control" id="price" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="quantity" className="form-label">Quantity</label>
+                                    <input type="text" name='quantity' value={product?.quantity || ''} onChange={handleChange} className="form-control" id="quantity" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="category" className="form-label">Category</label>
+                                    <input type="text" name='category' value={product?.category || ''} onChange={handleChange} className="form-control" id="category" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="description" className="form-label">Description</label>
+                                    <input type="text" name='description' value={product?.description || ''} onChange={handleChange} className="form-control" id="description" />
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="submit" className="btn btn-primary">Update</button>
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
