bd23d50b8ee53974b83224f66a044e28380f79d2
YoussefLMT <mcyoussef324@gmail.com>
2022-12-05 19:44:27 +0100

    modifications

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,30 +17,30 @@ const sidebarData = [
 
 function Sidebar() {
 
-    const[clicked, setClicked] = useState(false)
+    const [clicked, setClicked] = useState(false)
 
     const toggle = () => {
         setClicked(clicked => !clicked)
     }
-    
-    return (
-        <div className="sidebar" style={{ width: clicked ? '5%' : '18%'}}>
-            <section className="routes">
-                    {sidebarData.map((data) => (
-                        <NavLink to={data.path} key={data.name} className="link">
-                            <div className="icon">
-                                {data.icon}
-                            </div>
 
-                            <div className="text">
-                                {!clicked && data.name}
-                            </div>
-                        </NavLink>
-                    ))}
-                </section>
-                <div className="toggle">
+    return (
+        <div className="sidebar" style={{ width: clicked ? '5%' : '18%' }}>
+            <section className="routes">
+                {sidebarData.map((data) => (
+                    <NavLink to={data.path} key={data.name} className="link">
+                        <div className="icon">
+                            {data.icon}
+                        </div>
+
+                        <div className="text">
+                            {!clicked && data.name}
+                        </div>
+                    </NavLink>
+                ))}
+            </section>
+            <div className="toggle-icon">
                 <i className={clicked ? "fa-solid fa-arrow-right" : "fa-solid fa-arrow-left"} onClick={toggle}></i>
-                </div>
+            </div>
         </div>
     )
 }
