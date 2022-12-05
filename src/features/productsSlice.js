9d1dc8852b030ea855beb0628fe26d68923ec160
YoussefLMT <mcyoussef324@gmail.com>
2022-12-05 21:30:29 +0100

    add extra reducers

--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -4,7 +4,7 @@ import axiosInstance from '../axios'
 export const getProducts = createAsyncThunk('products/getProducts', async () => {
     try {
         const response = await axiosInstance.get('/products')
-        return response.data.products
+        console.log(response.data.products) 
     } catch (error) {
         console.log(error)
     }
@@ -20,6 +20,20 @@ const initialState = {
 const productsSlice = createSlice({
     name: "products",
     initialState,
+    extraReducers: (builder) => {
+        builder.addCase(getProducts.pending, (state) => {
+            state.loading = true
+        })
+        builder.addCase(getProducts.fulfilled, (state, action) => {
+            state.loading = false
+            state.error = false
+            state.products = action.payload
+        })
+        builder.addCase(getProducts.rejected, (state) => {
+            state.error = true
+            state.loading = false
+        })
+    }
 })
 
 export default productsSlice.reducer
