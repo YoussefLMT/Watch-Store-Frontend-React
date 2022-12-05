4a69010b8a9e3e7ed53f4d8a2e1f1806b83b869e
YoussefLMT <mcyoussef324@gmail.com>
2022-12-05 21:42:01 +0100

    add action buttons

--- src/components/ProductsTable.jsx
+++ src/components/ProductsTable.jsx
@@ -13,6 +13,7 @@ function ProductsTable(props) {
                         <th scope="col">Category</th>
                         <th scope="col">Description</th>
                         <th scope="col">Image</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -27,6 +28,10 @@ function ProductsTable(props) {
                                     <td>{product.category}</td>
                                     <td>{product.description}</td>
                                     <img src={`http://127.0.0.1:8000/${product.image}`}/>
+                                    <td>
+                                        <button type="button" class="btn btn-danger"><i class="fa-sharp fa-solid fa-trash"></i></button>
+                                        <button type="button" style={{marginLeft: '10px', color: 'white'}} data-bs-toggle="modal" data-bs-target="#updateUserModal" class="btn btn-warning"><i class="fa-sharp fa-solid fa-pen-to-square"></i></button>
+                                    </td>
                                 </tr>
                             )
                         })
