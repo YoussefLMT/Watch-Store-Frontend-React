24a1cf8c260fc9a64293a6acbf7574c874b3ac06
YoussefLMT <mcyoussef324@gmail.com>
2022-12-07 21:14:04 +0100

    add content

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 function Shop() {
-  return (
-    <div>Shop</div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <div class="shop">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 mx-auto mt-3">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="content">
+                </div>
+            </div>
+            <Footer />
+        </div >
+    )
 }
 
 export default Shop
