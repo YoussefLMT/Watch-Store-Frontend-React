db4e4a593ed8e12e79e854f4a966590cc9f2b4c0
YoussefLMT <mcyoussef324@gmail.com>
2022-12-07 18:40:39 +0100

    add content

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
-function Pagination() {
-  return (
-    <div>Pagination</div>
-  )
+function Pagination(props) {
+
+    const pageNumbers = []
+
+    for (let i = 1; i <= Math.ceil(props.totalProducts / props.productsPerPage); i++) {
+        pageNumbers.push(i)
+    }
+
+    return (
+        <nav>
+            <ul className="pagination">
+                {
+                    pageNumbers.map((number) => (
+                        <li key={number} className="page-item">
+                            <button className="page-link">{number}</button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </nav>
+    )
 }
 
 export default Pagination
