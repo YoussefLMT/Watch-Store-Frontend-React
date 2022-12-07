e8aa7151594703667901d24263fc69fa628f6fb0
YoussefLMT <mcyoussef324@gmail.com>
2022-12-07 18:53:34 +0100

    add content

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Product() {
-  return (
-    <div>Product</div>
-  )
+function Product(props) {
+    return (
+        <div class="box">
+            <div class="imgbox">
+                <img src={props.image} />
+            </div>
+            <div class="text">
+                <h3>{props.name}</h3>
+                <p>{props.price} DH</p>
+                <p>Quantity: {props.quantity}</p>
+            </div>
+        </div >
+    )
 }
 
 export default Product
