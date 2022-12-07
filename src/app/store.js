cf5b7b681c91bbd6420c2db6dd96c6b4f0dfac48
YoussefLMT <mcyoussef324@gmail.com>
2022-12-07 19:22:10 +0100

    register the slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/authSlice'
 import productsReducer from '../features/productsSlice'
+import latestProductsReducer from '../features/latestProductsSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productsReducer,
+    latestProducts: latestProductsReducer,
   },
 });
