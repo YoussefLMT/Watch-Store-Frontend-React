4874d579f869b486922df810c5c5b58cfd509ff8
YoussefLMT <mcyoussef324@gmail.com>
2022-12-08 21:18:52 +0100

    add loading

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -7,6 +7,7 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import { getProduct } from '../features/productsSlice'
 import './styles/productDetails.css'
+import ClipLoader from "react-spinners/ClipLoader"
 
 function ProductDetails() {
 
@@ -14,7 +15,7 @@ function ProductDetails() {
 
     const params = useParams()
 
-    const { product } = useSelector((state) => state.products)
+    const { product, loading } = useSelector((state) => state.products)
 
     const [quantity, setQuantity] = useState(1)
 
@@ -37,25 +38,34 @@ function ProductDetails() {
     return (
         <>
             <Navbar />
-            <div class="row d-flex justify-content-center">
-                <div class="col-md-4">
-                    <img class="details-img" src={'http://127.0.0.1:8000/' + product.image} />
+            <div class="p-container">
+                <div class="row d-flex justify-content-center">
+                    {
+                        loading ? <ClipLoader /> :
+
+                            <>
+                                <div class="col-md-4">
+                                    <img class="details-img" src={'http://127.0.0.1:8000/' + product.image} />
+                                </div>
+                                <div class="col-md-4">
+                                    <Link to="/">Go Back</Link><br /><br />
+                                    <h5>{product.name}</h5>
+                                    <p><span class="product-details-title">Price:</span> {product.price}DH</p>
+                                    <p><span class="product-details-title">Description:</span> {product.description}</p>
+                                    <p><span class="product-details-title">Quantity:</span> {product.quantity}</p>
+                                    <div class="quantity-toggle">
+                                        <button onClick={decrement}>&mdash;</button>
+                                        <input type="text" value={quantity} readonly />
+                                        <button onClick={increment}>&#xff0b;</button>
+                                    </div>
+                                    <br />
+                                    <button class="btn">Add Product To Cart</button>
+                                </div>
+                            </>
+                    }
+
                 </div>
-                <div class="col-md-4">
-                    <Link to="/">Go Back</Link><br /><br />
-                    <h5>{product.name}</h5>
-                    <p><span class="product-details-title">Price:</span> {product.price}DH</p>
-                    <p><span class="product-details-title">Description:</span> {product.description}</p>
-                    <p><span class="product-details-title">Quantity:</span> {product.quantity}</p>
-                    <div class="quantity-toggle">
-                        <button onClick={decrement}>&mdash;</button>
-                        <input type="text" value={quantity} readonly />
-                        <button onClick={increment}>&#xff0b;</button>
-                    </div>
-                    <br />
-                    <button class="btn">Add Product To Cart</button>
-                </div >
-            </div >
+            </div>
             <Footer />
         </>
     )
