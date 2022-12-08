a6258eb58d5faa04b2fd231ca66c2897a6b000bc
YoussefLMT <mcyoussef324@gmail.com>
2022-12-08 20:47:06 +0100

    display product

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import { getProduct } from '../features/productsSlice'
 
 function ProductDetails() {
+
+    const dispatch = useDispatch()
+
+    const params = useParams()
+
+    const { product } = useSelector((state) => state.products)
+
+    useEffect(() => {
+        dispatch(getProduct(params.id))
+    }, [dispatch])
     return (
         <>
             <Navbar />
-            <div v-else class="row d-flex justify-content-center">
+            <div class="row d-flex justify-content-center">
                 <div class="col-md-4">
-                    <img class="details-img" src="'http://127.0.0.1:8000/' + product.image" />
+                    <img class="details-img" src={'http://127.0.0.1:8000/' + product.image} />
                 </div>
                 <div class="col-md-4">
                     <router-link to="/">Go Back</router-link><br /><br />
