10445f5af67aab5b2c6a14f696af56e3ec8ba81a
YoussefLMT <mcyoussef324@gmail.com>
2022-12-08 20:29:45 +0100

    add filter products functunality

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -7,6 +7,7 @@ import Product from '../components/Product'
 import { getProducts } from '../features/productsSlice'
 import './styles/shop.css'
 import ClipLoader from "react-spinners/ClipLoader"
+import { useState } from 'react'
 
 function Shop() {
 
@@ -14,9 +15,16 @@ function Shop() {
 
     const { products, loading } = useSelector((state) => state.products)
 
+    const [category, setCategory] = useState('')
+
     useEffect(() => {
         dispatch(getProducts())
     }, [dispatch])
+
+    const filteredProducts = products.filter((product) => {
+        return product.category === category
+    })
+
 
     return (
         <div>
@@ -24,15 +32,27 @@ function Shop() {
             <div class="shop">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-4 mx-auto mt-3">
-
+                        <div class="col-md-4 mx-auto">
+                            <label for="category" class="form-label">Select a category</label>
+                            <select class="form-select" id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
+                                <option value="all">All</option>
+                                <option value="men">Men</option>
+                                <option value="women">Women</option>
+                                <option value="sport">Sport</option>
+                                <option value="smart">Smart</option>
+                                <option value="classic">Classic</option>
+                            </select>
                         </div>
                     </div>
                 </div>
                 <div class="content">
                     {
                         loading ? <ClipLoader /> :
-                            products.map((product) => {
+                            category === "all" ? products.map((product) => {
+                                return <Product image={`http://127.0.0.1:8000/${product.image}`} name={product.name} price={product.price} quantity={product.quantity} id={product.id} />
+                            }) : 
+
+                            filteredProducts.map((product) => {
                                 return <Product image={`http://127.0.0.1:8000/${product.image}`} name={product.name} price={product.price} quantity={product.quantity} id={product.id} />
                             })
                     }
