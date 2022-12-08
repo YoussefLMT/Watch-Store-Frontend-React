5556a321eef5df1d1dd5c9a06492c316f64fdc8f
YoussefLMT <mcyoussef324@gmail.com>
2022-12-08 18:29:48 +0100

    add log out to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import './styles/sidebar.css'
+import { logout } from '../features/authSlice'
+import { useDispatch } from 'react-redux';
+import Swal from 'sweetalert2';
 
 const sidebarData = [
     {
@@ -19,8 +22,34 @@ function Sidebar() {
 
     const [clicked, setClicked] = useState(false)
 
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+
     const toggle = () => {
         setClicked(clicked => !clicked)
+    }
+
+    const logOut = () => {
+        dispatch(logout())
+
+        Toast.fire({
+            icon: 'success',
+            title: 'Signed out successfully'
+        })
+
+        navigate("/login")
     }
 
     return (
@@ -38,6 +67,9 @@ function Sidebar() {
                     </NavLink>
                 ))}
             </section>
+            <div className="log-out">
+                <button onClick={logOut} className="btn btn-danger">Log out</button>
+            </div>
             <div className="toggle-icon">
                 <i className={clicked ? "fa-solid fa-arrow-right" : "fa-solid fa-arrow-left"} onClick={toggle}></i>
             </div>
